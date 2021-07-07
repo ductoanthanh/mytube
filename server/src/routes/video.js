e5498d54a69f9aebfedc39298da32c8be3eeb38e
Duc Toan Thanh <toan.thanh@columbiaroad.com>
2021-07-07 16:10:30 +0300

    Add recommended videos endpoint

--- server/src/routes/video.js
+++ server/src/routes/video.js
@@ -1,12 +1,48 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 function getVideoRoutes() {
   const router = express.Router();
 
+  router.get("/", getRecommendedVideos);
+
   return router;
 }
 
-async function getRecommendedVideos(req, res) {}
+async function getVideoViews(videos) {
+  for (const video of videos) {
+    const views = await prisma.view.count({
+      where: {
+        videoId: {
+          equals: video.id
+        }
+      }
+    });
+    video.views = views;
+  }
+  return videos;
+}
+
+async function getRecommendedVideos(req, res) {
+  let videos = await prisma.video.findMany({
+    include: {
+      user: true
+    },
+    orderBy: {
+      createdAt: "desc"
+    }
+  });
+
+  if (!videos.length) {
+    return res.status(200).json({ videos });
+  }
+
+  videos = await getVideoViews(videos);
+
+  res.status(200).json({ videos });
+}
 
 async function getTrendingVideos(req, res) {}
 
