490a0ed8ffe5232acf7fa5f0009ae1f4e3f4b2f0
Duc Toan Thanh <toan.thanh@columbiaroad.com>
2021-07-12 17:16:36 +0300

    Add getAuthUser middleware & add video view

--- server/src/routes/video.js
+++ server/src/routes/video.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
-import { protect } from "../middleware/authorization";
+import { getAuthUser, protect } from "../middleware/authorization";
 
 const prisma = new PrismaClient();
 
@@ -13,6 +13,7 @@ function getVideoRoutes() {
   router.post("/", protect, addVideo);
   router.post("/:videoId/comments", protect, addComment);
   router.delete("/:videoId/comments/:commentId", protect, deleteComment);
+  router.get("/:videoId/view", getAuthUser, addVideoView);
 
   return router;
 }
@@ -188,7 +189,49 @@ async function deleteComment(req, res) {
   res.status(200).json({});
 }
 
-async function addVideoView(req, res, next) {}
+async function addVideoView(req, res, next) {
+  const video = await prisma.video.findUnique({
+    where: {
+      id: req.params.videoId
+    }
+  });
+
+  if (!video) {
+    return next({
+      message: `No video found with id: "${req.params.videoId}"`,
+      statusCode: 404
+    });
+  }
+
+  if (req.user) {
+    await prisma.view.create({
+      data: {
+        video: {
+          connect: {
+            id: req.params.videoId
+          }
+        },
+        user: {
+          connect: {
+            id: req.user.id
+          }
+        }
+      }
+    });
+  } else {
+    await prisma.view.create({
+      data: {
+        video: {
+          connect: {
+            id: req.params.videoId
+          }
+        }
+      }
+    });
+  }
+
+  res.status(200).json({});
+}
 
 async function likeVideo(req, res, next) {}
 
