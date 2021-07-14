4acb5c2620df8200360ca111ef4ae4e889760fc5
Duc Toan Thanh <toan.thanh@columbiaroad.com>
2021-07-14 16:42:40 +0300

    Add Google OAuth

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,17 +1,32 @@
 import React from "react";
 import Button from "../styles/Auth";
 import { SignInIcon } from "./Icons";
+import { GoogleLogin } from "react-google-login";
+import { authenticate } from "../utils/api-client";
 
 function GoogleAuth() {
   return (
-    <Button tabIndex={0} type="button">
-      <span className="outer">
-        <span className="inner">
-          <SignInIcon />
-        </span>
-        sign in
-      </span>
-    </Button>
+    <GoogleLogin
+      clientId="724096889503-htt3jh98033ccm54s8oe9h4e4h1h7lcj.apps.googleusercontent.com"
+      cookiePolicy="single_host_origin"
+      onSuccess={authenticate}
+      onFailure={authenticate}
+      render={(renderProps) => (
+        <Button
+          tabIndex={0}
+          type="button"
+          onClick={renderProps.onClick}
+          disabled={renderProps.disabled}
+        >
+          <span className="outer">
+            <span className="inner">
+              <SignInIcon />
+            </span>
+            sign in
+          </span>
+        </Button>
+      )}
+    />
   );
 }
 
