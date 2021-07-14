72de9750917c3a3dbbde018d25cad52367a6839a
Duc Toan Thanh <toan.thanh@columbiaroad.com>
2021-07-14 16:55:05 +0300

    Add useAuth hook

--- client/src/components/AppProviders.js
+++ client/src/components/AppProviders.js
@@ -3,14 +3,17 @@ import { ThemeProvider } from "styled-components";
 import { BrowserRouter as Router } from "react-router-dom";
 import GlobalStyle from "../styles/GlobalStyle";
 import { darkTheme } from "../styles/theme";
+import { AuthProvider } from "../context/auth-context";
 
 function AppProviders({ children }) {
   return (
     <Router>
-      <ThemeProvider theme={darkTheme}>
-        <GlobalStyle />
-        {children}
-      </ThemeProvider>
+      <AuthProvider>
+        <ThemeProvider theme={darkTheme}>
+          <GlobalStyle />
+          {children}
+        </ThemeProvider>
+      </AuthProvider>
     </Router>
   );
 }
