0fa94442c87822f1e30477d1ab8f1a54cfcdb36f
Duc Toan Thanh <toan.thanh@columbiaroad.com>
2021-07-14 18:25:28 +0300

    Init uploading video to Cloudinary & snackbar noti

--- client/src/components/UploadVideo.js
+++ client/src/components/UploadVideo.js
@@ -1,7 +1,33 @@
 import React from "react";
 import { UploadIcon } from "./Icons";
+import { uploadMedia } from "../utils/upload-media";
+import { useSnackbar } from "react-simple-snackbar";
 
 function UploadVideo() {
+  const [openSnackbar] = useSnackbar();
+
+  async function handleUploadVideo(event) {
+    event.persist();
+    const file = event.target.files[0];
+
+    if (file) {
+      const fileSize = file.size / 1000000;
+
+      if (fileSize > 50) {
+        return openSnackbar("Video file should be less than 50MB");
+      }
+
+      const url = await uploadMedia({
+        type: "video",
+        file,
+        preset: "tnrk1id4"
+      });
+
+      console.log(url);
+      event.target.value = "";
+    }
+  }
+
   return (
     <div>
       <label htmlFor="video-upload">
@@ -12,6 +38,7 @@ function UploadVideo() {
         id="video-upload"
         type="file"
         accept="video/*"
+        onChange={handleUploadVideo}
       />
     </div>
   );
