c2535acb8a496be4a17953847ddca3cc5a31d7ec
Duc Toan Thanh <toan.thanh@columbiaroad.com>
2021-07-16 15:59:28 +0300

    Finalise channel tab

--- client/src/components/VideoCard.js
+++ client/src/components/VideoCard.js
@@ -5,7 +5,7 @@ import Wrapper from "../styles/VideoCard";
 import { formatCreatedAt } from "../utils/date";
 import DeleteVideoDropdown from "./DeleteVideoDropdown";
 
-function VideoCard({ video }) {
+function VideoCard({ video, hideAvatar, noUsername }) {
   return (
     <Wrapper>
       <Link to={`/watch/${video.id}`}>
@@ -13,16 +13,23 @@ function VideoCard({ video }) {
       </Link>
       <div className="video-info-container">
         <div className="channel-avatar">
-          <Avatar
-            style={{ marginRight: "0.8rem" }}
-            src={video.user.avatar}
-            alt={`${video.user.username}'s channel avatar`}
-          />
+          {!hideAvatar && (
+            <Avatar
+              style={{ marginRight: "0.8rem" }}
+              src={video.user.avatar}
+              alt={`${video.user.username}'s channel avatar`}
+            />
+          )}
         </div>
         <div className="video-info">
           <Link to={`/watch/${video.id}`}>
             <h4 className="truncate">{video.title}</h4>
           </Link>
+          {!noUsername && (
+            <Link to={`/channel/${video.user.id}`}>
+              <span className="secondary">{video.user.username}</span>
+            </Link>
+          )}
           <p className="secondary leading-4">
             <span>{video.views} views</span> <span>•</span>{" "}
             <span>{formatCreatedAt(video.createdAt)}</span>
